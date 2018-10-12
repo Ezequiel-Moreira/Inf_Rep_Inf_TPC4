e2ccf69615173653c6713528c2d978ab6e905fe7
Ezequiel-Moreira <99ezequiel90@gmail.com>
2018-10-12 11:25:55 +0100

    third commit: adição de index na página principal

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var http = require('http')
+var fs = require('fs')
 var server=7000
 
 http.createServer( (req,res) => {
     res.writeHead(200,{'Content-type':'text/html'})
-    res.write("Olá!")
-    res.end()
+
+    fs.readFile('./dados/index.html',(erro,dados)=>{
+        
+        if(!erro){
+            res.write(dados)
+        }else{
+            res.write(erro)
+        }
+
+        res.end()
+    })
+    
 }).listen(server,()=>{
    console.log('Servidor á escuta na porta ' + server) 
 })
