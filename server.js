96448825a5c3aae11cb3c2cfbf8aca8f939f9582
Ezequiel-Moreira <99ezequiel90@gmail.com>
2018-10-12 11:55:29 +0100

    fourth commit: adicionar alguma lógica

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 var http = require('http')
 var fs = require('fs')
+var url = require('url')
 var server=7000
 
 http.createServer( (req,res) => {
     res.writeHead(200,{'Content-type':'text/html'})
 
-    fs.readFile('./dados/index.html',(erro,dados)=>{
-        
-        if(!erro){
-            res.write(dados)
-        }else{
-            res.write(erro)
-        }
+    var parsedUrl = url.parse(req.url,true)
+    var q = parsedUrl.query    
+    if(q.id){
+        fs.readFile('./dados/html/obra' + q.id + '.html', (erro,dados)=>{
+            if(!erro){
+                res.write(dados)
+            }else{
+                //res.write(erro)
+                console.log(erro)
+            }
+    
+            res.end()    
+        })
+    }else{
+        fs.readFile('./dados/index.html',(erro,dados)=>{
+            
+            if(!erro){
+                res.write(dados)
+            }else{
+                res.write(erro)
+            }
 
-        res.end()
-    })
+            res.end()
+        })
+
+    }    
     
 }).listen(server,()=>{
    console.log('Servidor á escuta na porta ' + server) 
